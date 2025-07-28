eef666491d87ade86059482f51544ca1f991568a
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2025-07-28 14:14:13 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,47 @@
 // service-worker.js
+
+const CACHE_NAME = 'weight-tracker-cache-v1';
+const URLS_TO_CACHE = [
+  './index.html',
+  './style.css',
+  './app.js',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+// Install event: cache assets
 self.addEventListener('install', (event) => {
   console.log('Service Worker installing...');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
   self.skipWaiting();
 });
 
+// Activate event: clean up old caches
 self.addEventListener('activate', (event) => {
   console.log('Service Worker activating...');
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
 });
 
+// Fetch event: serve from cache or fetch from network
 self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      return cachedResponse || fetch(event.request);
+    })
+  );
 });
